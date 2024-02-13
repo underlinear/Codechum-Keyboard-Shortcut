5f4d0cf7084679bd09ed673ffbba8232b31e77ad
underlinear <arduouschore@gmail.com>
2024-02-13 17:44:00 +0800

    Add comments finally

--- codechum_shortcut.js
+++ codechum_shortcut.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         CodeChum Shortcuts v2
+// @name         CodeChum Shortcuts
 // @namespace    http://tampermonkey.net/
 // @version      2024-02-13
-// @description  Codechum shortcuts
+// @description  Codechum Shortcuts to save time
 // @author       weirdo olfu student
 // @match        *://*.codechum.com/*
 // @icon         https://underlinear.github.io/Paint/images/icons/pencil.png
 // @grant        none
 // ==/UserScript==
 
-// Customize your codechum shortcuts on user_shortcuts_array!
+// Customize your codechum shortcuts on userShortcutsArray!
 // Add %CARET to determine where your text cursor will land after pasting.
 
 'use strict';
@@ -69,29 +69,48 @@ window.addEventListener('keydown', (e) => {
 })
 
 function handleKeyDown(event){
+    // For some reason, Safari immediately converts 'Shift' + 'Number' into its special symbol
+    // Unlike Chrome, which treats number presses as its actual number regardless of the 'Shift' pressed on Mac
     let keyPressed = ")!@#$%^&*(".indexOf(event.key) !== -1 ? ")!@#$%^&*(".indexOf(event.key) : !isNaN(Number(event.key)) ? Number(event.key) : -1;
+
+    // Detect for Alt + Shift or Control + Shift and a number, canAppend to delay pasting for 0.4s
     if(canAppend && ((event.altKey && event.shiftKey) || (event.ctrlKey && event.shiftKey)) && keyPressed !== -1 ) {
         pasteText(keyPressed);
 
         canAppend = false;
         setTimeout(() => {
             canAppend = true;
-        }, 1000);
+        }, 400);
     }
 }
 
 function pasteText(keyPressed){
+    // CodeChum's class for the line where the caret/text cursor is
     const cmActiveLine = document.querySelector('.cm-activeLine');
-    const cursorPosition = getCursorPosition();
+
+    // Get caret position to slice text accordingly later
+    const cursorPosition = getCaretPosition();
+
+    // Check for indentation(tabs/spaces) at the start of line and use that as context
     const matchedSpaces = cmActiveLine.textContent.match(/^(\s*)/);
+
+    // Determine how many spaces there are at the front on the current active line
     const spacesCount = matchedSpaces[1].length;
+
     let selectedText = userShortcutsArray[Number(keyPressed)];
+
+    // Add indentation if spaces were found at the current active line
     if(spacesCount > 0){
         selectedText = addIndentation(selectedText, spacesCount);
     }
+
+    // Add preceding text if any + text to paste + succeeding text if any
     const appendedText = cmActiveLine.textContent.slice(0, cursorPosition)
                         + selectedText
                         + cmActiveLine.textContent.slice(cursorPosition);
+
+    // cHeck for custom caret and sets caret to where "%CARET" was found
+    // Else, set caret to the end of text pasted
     if(selectedText.includes("%CARET")){
         cmActiveLine.textContent = appendedText.replace("%CARET",'');
         setCaretPosition(cursorPosition + selectedText.indexOf("%CARET"));
@@ -102,22 +121,23 @@ function pasteText(keyPressed){
     }
 }
 
+// Add spaces to each line if necessary
 function addIndentation(selectedText, spacesCount){
     let lineSeparatedText = selectedText.split("\n");
     lineSeparatedText[0] = lineSeparatedText[0] + "\n";
     for(let i = 1; i < lineSeparatedText.length; i++){
-        lineSeparatedText[i] = ' '.repeat(spaceCount) + lineSeparatedText[i] + (i !== lineSeparatedText.length - 1 ? "\n" : '');
+        lineSeparatedText[i] = ' '.repeat(spacesCount) + lineSeparatedText[i] + (i !== lineSeparatedText.length - 1 ? "\n" : '');
     }
     return lineSeparatedText.join("");
 }
 
-// Not familiar with handling contentEditable carets in HTML and couldn't learn it myself so I had to resort to code online (╥ ᴗ ╥)
+// Not familiar with handling contentEditable carets in HTML and wasn't able to learn it myself so I had to resort to code online (╥ ᴗ ╥)
 
 // Code from Soubriquet (https://stackoverflow.com/a/54333903)
-function getCursorPosition() {
-    var sel = document.getSelection();
+function getCaretPosition() {
+    let sel = document.getSelection();
     sel.modify("extend", "backward", "paragraphboundary");
-    var pos = sel.toString().length;
+    let pos = sel.toString().length;
     if(sel.anchorNode != undefined) sel.collapseToEnd();
 
     return pos;
@@ -133,3 +153,5 @@ function setCaretPosition(position) {
     sel.removeAllRanges();
     sel.addRange(range);
 }
+
+//TODO: Add HTML GUI
